48f5cce09ff827a4b80d8fa4a67b62191ab7fd10
Wilco Omolo <wilcomilcinovic@gmail.com>
2026-06-02 09:58:01 +0300

    redirects and netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/WilcoPortfolio',
+  base: process.env.NODE_ENV === 'production' && !process.env.NETLIFY ? '/WilcoPortfolio/' : '/',
 })
 
